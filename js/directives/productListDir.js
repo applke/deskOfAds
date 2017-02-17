7aae8912f3266ceaa59ef6912fb5597f80b3a69f
Yaroslav Golopapa <applkeee@gmail.com>
2017-02-17 11:43:30 +0200

    Added Bootstrap and Cart!

--- js/directives/productListDir.js
+++ js/directives/productListDir.js
@@ -1,12 +1,15 @@
-deskApp.directive('productList',function () {
+deskApp.directive('productList',['cart','$filter',function (cart,$filter) {
     return {
         scope:{
             product:'='
-        },
-        link: function (scope) {
-
+        },link:function (scope) {
+            scope.inCart = $filter('cartExist')(scope.product);
+            scope.addToCart = function () {
+                cart.addCart(scope.product);
+                scope.inCart = true;
+            };
         },
         templateUrl: "./template/singleProduct.html"
 
     };
-});
+}]);
--- js/filters/productFilters.js
+++ js/filters/productFilters.js
@@ -1,6 +1,14 @@
 /**
  * Created by asus on 17.02.2017.
  */
-deskApp.filter('slice',function () {
-    
-});
+deskApp.filter('cartExist',['cart',function (cart) {
+    return function (element) {
+        var i=0;
+        var c = cart.getCart();
+        for(i;i<c.length;i++){
+            if(c[i] === element.productID)
+                return true;
+        }
+        return false;
+    }
+}]);
